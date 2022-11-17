6bc629baa058cf480385934431b8707666b9aeda
mverzi <maggie.verzi@gmail.com>
2022-11-17 11:01:51 -0500

    First commit

---
+++ stringRepeat.js
@@ -0,0 +1,9 @@
+/**
+ * This function returns a string s repeated n number of times
+ * @param {integer} n 
+ * @param {string} s 
+ * @returns s repeated n times
+ */
+function repeatStr (n, s) {
+    return s.repeat(n)
+  }
